48efa1f51eb1184818b5e35c4ff3c1a3e3790bc0
melonwark <102952154+melonwark@users.noreply.github.com>
2022-06-14 20:16:51 +0300

    Add files via upload
    JS HW

---
+++ @Exiteli #4.js
@@ -0,0 +1,26 @@
+let age_2 = 18;
+
+let age_3 = 60;
+
+let age = prompt('How old are you?')
+
+const check_age= (age = prompt('How old are you?'))=> {
+    age=+age
+
+    if(!isNaN(age) && typeof(age) === 'number' ) {
+
+    if(age < age_2) {
+         console.log("You don't have access cause your age is " + age, 'It\'s less then '+ age_2)
+    } else if(age >= age_2 && age< age_3) {
+         console.log('Welcome!')
+    } else if(age > age_3) {
+         console.log('Keep calm and look Culture channel')
+    } else {
+        console.log('Technical work')
+    }
+} else {
+    console.log('Error: wrong data type')
+}
+}
+
+check_age()
